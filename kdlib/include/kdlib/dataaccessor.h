83dcf31ca037df83364953d49f0a63ef29543d04
SND\ussrhero_cp <SND\ussrhero_cp@9b283d60-5439-405e-af05-b73fd8c4d996>
2016-11-18 21:59:36 +0000

    dataaccessor reworked
    git-svn-id: https://kdlibcpp.svn.codeplex.com/svn@91062 9b283d60-5439-405e-af05-b73fd8c4d996

--- kdlib/include/kdlib/dataaccessor.h
+++ kdlib/include/kdlib/dataaccessor.h
@@ -19,29 +19,67 @@ class DataAccessor
 public:
 
     virtual size_t getLength() const = 0;
+
     virtual unsigned char readByte(size_t pos=0) const = 0;
+    virtual void writeByte(unsigned char value, size_t pos=0) = 0;
+
     virtual char readSignByte(size_t pos=0) const = 0;
+    virtual void writeSignByte(char value, size_t pos=0) = 0;
+
     virtual unsigned short readWord(size_t pos=0) const = 0;
+    virtual void writeWord(unsigned short value, size_t pos=0) = 0;
+
     virtual short readSignWord(size_t pos=0) const = 0;
+    virtual void writeSignWord(short value, size_t pos=0) = 0;
+
     virtual unsigned long readDWord(size_t pos=0) const = 0;
+    virtual void writeDWord(unsigned long value, size_t pos) = 0;
+
     virtual long readSignDWord(size_t pos=0) const = 0;
+    virtual void writeSignDWord(long value, size_t pos=0) = 0;
+
     virtual unsigned long long readQWord(size_t pos=0) const = 0;
+    virtual void writeQWord(unsigned long long value, size_t pos=0) = 0;
+
     virtual long long readSignQWord(size_t pos=0) const = 0;
+    virtual void writeSignQWord(long long value, size_t pos=0) = 0;
+
     virtual float readFloat(size_t pos=0) const = 0;
+    virtual void writeFloat(float value, size_t pos=0) = 0;
+
     virtual double readDouble(size_t pos=0) const = 0;
+    virtual void writeDouble(double value, size_t pos=0) = 0;
 
-    virtual void readBytes( std::vector<unsigned char>&  dataRange, size_t count, size_t  pos=0) const = 0;
+    virtual void readBytes( std::vector<unsigned char>&  dataRange, size_t count, size_t pos=0) const = 0;
+    virtual void writeBytes( const std::vector<unsigned char>&  dataRange, size_t pos=0) = 0;
+
     virtual void readWords( std::vector<unsigned short>&  dataRange, size_t count, size_t  pos=0) const = 0;
-    virtual void readDWords( std::vector<unsigned long>&  dataRange, size_t count, size_t  pos=0) const = 0;
-    virtual void readQWords( std::vector<unsigned long long>&  dataRange, size_t count, size_t  pos=0) const = 0;
-    virtual void readSignBytes( std::vector<char>&  dataRange, size_t count, size_t  pos=0) const = 0;
-    virtual void readSignWords( std::vector<short>&  dataRange, size_t count, size_t  pos=0) const = 0;
-    virtual void readSignDWords( std::vector<long>&  dataRange, size_t count, size_t  pos=0) const = 0;
-    virtual void readSignQWords( std::vector<long long>&  dataRange, size_t count, size_t  pos=0) const = 0;
-    virtual void readFloats( std::vector<float>&  dataRange, size_t count, size_t  pos=0) const = 0;
-    virtual void readDoubles( std::vector<double>&  dataRange, size_t count, size_t  pos=0) const = 0;
+    virtual void writeWords( const std::vector<unsigned short>&  dataRange, size_t pos=0)  = 0;
 
-    virtual DataAccessorPtr clone(size_t count, size_t  pos=0) = 0;
+    virtual void readDWords( std::vector<unsigned long>&  dataRange, size_t count, size_t  pos=0) const = 0;
+    virtual void writeDWords( const std::vector<unsigned long>&  dataRange, size_t  pos=0)  = 0;
+
+    virtual void readQWords( std::vector<unsigned long long>&  dataRange, size_t count, size_t  pos=0) const = 0;
+    virtual void writeQWords( const std::vector<unsigned long long>&  dataRange, size_t  pos=0) = 0;
+
+    virtual void readSignBytes( std::vector<char>&  dataRange, size_t count, size_t  pos=0) const = 0;
+    virtual void writeSignBytes( const std::vector<char>&  dataRange, size_t  pos=0) = 0;
+
+    virtual void readSignWords( std::vector<short>&  dataRange, size_t count, size_t  pos=0) const = 0;
+    virtual void writeSignWords( const std::vector<short>&  dataRange, size_t  pos=0) = 0;
+
+    virtual void readSignDWords( std::vector<long>&  dataRange, size_t count, size_t  pos=0) const = 0;
+    virtual void writeSignDWords( const std::vector<long>&  dataRange, size_t  pos=0) = 0;
+
+    virtual void readSignQWords( std::vector<long long>&  dataRange, size_t count, size_t  pos=0) const = 0;
+    virtual void writeSignQWords( const std::vector<long long>&  dataRange, size_t  pos=0) = 0;
+
+    virtual void readFloats( std::vector<float>&  dataRange, size_t count, size_t  pos=0) const = 0;
+    virtual void writeFloats( const std::vector<float>&  dataRange, size_t  pos=0) = 0;
+
+    virtual void readDoubles( std::vector<double>&  dataRange, size_t count, size_t  pos=0) const = 0;
+    virtual void writeDoubles( const std::vector<double>&  dataRange, size_t  pos=0) = 0;
+
     virtual MEMOFFSET_64 getAddress() const = 0;
     virtual VarStorage getStorageType() const = 0;
     virtual std::wstring getRegisterName() const = 0;
@@ -49,11 +87,12 @@ public:
 
 ///////////////////////////////////////////////////////////////////////////////
 
-DataAccessorPtr getMemoryAccessor( MEMOFFSET_64  offset, size_t length);
-DataAccessorPtr getCacheAccessor(const std::vector<char>& buffer);
-DataAccessorPtr getVariantAccessor(const NumVariant& var);
-DataAccessorPtr  getRegisterAccessor(const NumVariant& registerValue, const std::wstring& registerName);
 DataAccessorPtr  getEmptyAccessor();
+DataAccessorPtr getMemoryAccessor( MEMOFFSET_64  offset, size_t length);
+//DataAccessorPtr getCacheAccessor(const std::vector<char>& buffer);
+//DataAccessorPtr getVariantAccessor(const NumVariant& var);
+DataAccessorPtr  getRegisterAccessor(const NumVariant& registerValue, const std::wstring& registerName);
+
 
 ///////////////////////////////////////////////////////////////////////////////
 
--- kdlib/source/dataaccessorimpl.h
+++ kdlib/source/dataaccessorimpl.h
@@ -10,7 +10,7 @@ namespace kdlib {
 
 ///////////////////////////////////////////////////////////////////////////////
 
-class EmptyAccessor : public DataAccessor, boost::noncopyable
+class EmptyAccessor : public DataAccessor
 {
 private:
 
@@ -24,7 +24,22 @@ private:
         throw DbgException("data accessor no data");
     }
 
+    virtual void writeByte(unsigned char value, size_t pos=0)
+    {
+        throw DbgException("data accessor no data");
+    }
+
+    virtual void writeByte(size_t pos=0) 
+    {
+        throw DbgException("data accessor no data");
+    }
+
     virtual char readSignByte(size_t pos = 0) const
+    {
+        throw DbgException("data accessor no data");
+    }
+
+    virtual void writeSignByte(char value, size_t pos=0) 
     {
         throw DbgException("data accessor no data");
     }
@@ -34,7 +49,17 @@ private:
         throw DbgException("data accessor no data");
     }
 
+    virtual void writeWord(unsigned short value, size_t pos=0) 
+    {
+        throw DbgException("data accessor no data");
+    }
+
     virtual short readSignWord(size_t pos = 0) const
+    {
+        throw DbgException("data accessor no data");
+    }
+
+    virtual void writeSignWord(short value, size_t pos=0)
     {
         throw DbgException("data accessor no data");
     }
@@ -44,7 +69,17 @@ private:
         throw DbgException("data accessor no data");
     }
 
+    virtual void writeDWord(unsigned long value, size_t pos)
+    {
+        throw DbgException("data accessor no data");
+    }
+
     virtual long readSignDWord(size_t pos = 0) const
+    {
+        throw DbgException("data accessor no data");
+    }
+
+    virtual void writeSignDWord(long value, size_t pos=0) 
     {
         throw DbgException("data accessor no data");
     }
@@ -54,7 +89,17 @@ private:
         throw DbgException("data accessor no data");
     }
 
+    virtual void writeQWord(unsigned long long value, size_t pos=0) 
+    {
+        throw DbgException("data accessor no data");
+    }
+
     virtual long long readSignQWord(size_t pos = 0) const
+    {
+        throw DbgException("data accessor no data");
+    }
+
+    virtual void writeSignQWord(long long value, size_t pos=0) 
     {
         throw DbgException("data accessor no data");
     }
@@ -64,7 +109,17 @@ private:
         throw DbgException("data accessor no data");
     }
 
+    virtual void writeFloat(float value, size_t pos=0)
+    {
+        throw DbgException("data accessor no data");
+    }
+
     virtual double readDouble(size_t pos = 0) const
+    {
+        throw DbgException("data accessor no data");
+    }
+
+    virtual void writeDouble(double value, size_t pos=0) 
     {
         throw DbgException("data accessor no data");
     }
@@ -74,7 +129,17 @@ private:
         throw DbgException("data accessor no data");
     }
 
+    virtual void writeBytes( const std::vector<unsigned char>&  dataRange, size_t pos=0)
+    {
+        throw DbgException("data accessor no data");
+    }
+
     virtual void readWords(std::vector<unsigned short>&  dataRange, size_t count, size_t  pos = 0) const
+    {
+        throw DbgException("data accessor no data");
+    }
+
+    virtual void writeWords( const std::vector<unsigned short>&  dataRange, size_t pos=0)
     {
         throw DbgException("data accessor no data");
     }
@@ -84,7 +149,17 @@ private:
         throw DbgException("data accessor no data");
     }
 
+    virtual void writeDWords( const std::vector<unsigned long>&  dataRange, size_t  pos=0) 
+    {
+        throw DbgException("data accessor no data");
+    }
+
     virtual void readQWords(std::vector<unsigned long long>&  dataRange, size_t count, size_t  pos = 0) const
+    {
+        throw DbgException("data accessor no data");
+    }
+
+    virtual void writeQWords( const std::vector<unsigned long long>&  dataRange, size_t  pos=0) 
     {
         throw DbgException("data accessor no data");
     }
@@ -94,7 +169,17 @@ private:
         throw DbgException("data accessor no data");
     }
 
+    virtual void writeSignBytes( const std::vector<char>&  dataRange, size_t  pos=0)
+    {
+        throw DbgException("data accessor no data");
+    }
+
     virtual void readSignWords(std::vector<short>&  dataRange, size_t count, size_t  pos = 0) const
+    {
+        throw DbgException("data accessor no data");
+    }
+
+    virtual void writeSignWords( const std::vector<short>&  dataRange, size_t  pos=0) 
     {
         throw DbgException("data accessor no data");
     }
@@ -104,7 +189,17 @@ private:
         throw DbgException("data accessor no data");
     }
 
+    virtual void writeSignDWords( const std::vector<long>&  dataRange, size_t  pos=0)
+    {
+        throw DbgException("data accessor no data");
+    }
+
     virtual void readSignQWords(std::vector<long long>&  dataRange, size_t count, size_t  pos = 0) const
+    {
+        throw DbgException("data accessor no data");
+    }
+
+    virtual void writeSignQWords( const std::vector<long long>&  dataRange, size_t  pos=0) 
     {
         throw DbgException("data accessor no data");
     }
@@ -114,12 +209,17 @@ private:
         throw DbgException("data accessor no data");
     }
 
+    virtual void writeFloats( const std::vector<float>&  dataRange, size_t  pos=0) 
+    {
+        throw DbgException("data accessor no data");
+    }
+
     virtual void readDoubles(std::vector<double>&  dataRange, size_t count, size_t  pos = 0) const
     {
         throw DbgException("data accessor no data");
     }
 
-    virtual DataAccessorPtr clone(size_t count, size_t  pos = 0)
+    virtual void writeDoubles( const std::vector<double>&  dataRange, size_t  pos=0) 
     {
         throw DbgException("data accessor no data");
     }
@@ -162,7 +262,16 @@ private:
     {
         if (pos >= m_length)
             throw DbgException("memory accessor range error");
+
         return ptrByte(m_begin + pos);
+    }
+
+    virtual void writeByte(unsigned char value, size_t pos=0) 
+    {
+        if (pos >= m_length)
+            throw DbgException("memory accessor range error");
+
+        setByte(m_begin + pos, value);
     }
 
     virtual char readSignByte(size_t pos = 0) const
@@ -172,138 +281,284 @@ private:
         return ptrSignByte(m_begin + pos);
     }
 
+    virtual void writeSignByte(char value, size_t pos=0)
+    {
+        if (pos >= m_length)
+            throw DbgException("memory accessor range error");
+        setSignByte(m_begin + pos, value);
+    }
+
     virtual unsigned short readWord(size_t pos = 0) const
     {
-        if (pos * sizeof(unsigned short) >= m_length - (sizeof(unsigned short) - 1))
+        if (pos >= m_length / sizeof(unsigned short) )
             throw DbgException("memory accessor range error");
         return ptrWord(m_begin + pos * sizeof(unsigned short));
     }
 
+    virtual void writeWord(unsigned short value, size_t pos=0)
+    {
+        if (pos >= m_length / sizeof(unsigned short) )
+            throw DbgException("memory accessor range error");
+        setWord(m_begin + pos * sizeof(unsigned short), value);
+    }
+
     virtual short readSignWord(size_t pos = 0) const
     {
-        if (pos * sizeof(short) >= m_length - (sizeof(short) - 1))
+        if (pos  >= m_length / sizeof(short) )
             throw DbgException("memory accessor range error");
         return ptrSignWord(m_begin + pos * sizeof(short));
     }
 
+    virtual void writeSignWord(short value, size_t pos=0)
+    {
+        if (pos >= m_length / sizeof(short) )
+            throw DbgException("memory accessor range error");
+        setSignWord(m_begin + pos * sizeof(short), value);
+    }
+
     virtual unsigned long readDWord(size_t pos = 0) const
     {
-        if (pos * sizeof(unsigned long) >= m_length - (sizeof(unsigned long) - 1))
+        if (pos >= m_length / sizeof(unsigned long) )
             throw DbgException("memory accessor range error");
         return ptrDWord(m_begin + pos * sizeof(unsigned long));
     }
 
+    virtual void writeDWord(unsigned long value, size_t pos)
+    {
+        if (pos >= m_length / sizeof(unsigned long) )
+            throw DbgException("memory accessor range error");
+        setDWord(m_begin + pos * sizeof(unsigned long), value);
+    }
+
     virtual long readSignDWord(size_t pos = 0) const
     {
-        if (pos * sizeof(long) >= m_length - (sizeof(long) - 1))
+        if (pos >= m_length /sizeof(long) )
             throw DbgException("memory accessor range error");
         return ptrSignDWord(m_begin + pos * sizeof(long));
     }
 
+    virtual void writeSignDWord(long value, size_t pos=0)
+    {
+        if (pos >= m_length /sizeof(long) )
+            throw DbgException("memory accessor range error");
+        setSignDWord(m_begin + pos * sizeof(long), value);
+    }
+
     virtual unsigned long long readQWord(size_t pos = 0) const
     {
-        if (pos * sizeof(unsigned long long) >= m_length - (sizeof(unsigned long long) - 1))
+        if (pos >= m_length / sizeof(unsigned long long) )
             throw DbgException("memory accessor range error");
         return ptrQWord(m_begin + pos * sizeof(unsigned long long));
     }
 
+    virtual void writeQWord(unsigned long long value, size_t pos=0)
+    {
+        if (pos >= m_length / sizeof(unsigned long long) )
+            throw DbgException("memory accessor range error");
+        setQWord(m_begin + pos * sizeof(unsigned long long), value);
+    }
+
     virtual long long readSignQWord(size_t pos = 0) const
     {
-        if (pos * sizeof(long long) >= m_length - (sizeof(long long) - 1))
+        if ( pos >= m_length / sizeof(long long) )
             throw DbgException("memory accessor range error");
         return ptrSignQWord(m_begin + pos * sizeof(long long));
     }
 
+    virtual void writeSignQWord(long long value, size_t pos=0) 
+    {
+        if ( pos >= m_length / sizeof(long long) )
+            throw DbgException("memory accessor range error");
+        setSignQWord(m_begin + pos * sizeof(long long), value);
+    }
+
     virtual float readFloat(size_t pos = 0) const
     {
-        if (pos * sizeof(float) >= m_length - (sizeof(float) - 1))
+        if (pos >= m_length / sizeof(float) )
             throw DbgException("memory accessor range error");
         return ptrSingleFloat(m_begin + pos * sizeof(float));
     }
 
+    virtual void writeFloat(float value, size_t pos=0) 
+    {
+        if (pos >= m_length / sizeof(float) )
+            throw DbgException("memory accessor range error");
+
+        setSingleFloat(m_begin + pos * sizeof(float), value);
+    }
+
     virtual double readDouble(size_t pos = 0) const
     {
-        if (pos * sizeof(double) >= m_length - (sizeof(double) - 1))
+        if (pos >= m_length / sizeof(double) )
             throw DbgException("memory accessor range error");
         return ptrDoubleFloat(m_begin + pos * sizeof(double));
     }
 
-    virtual void readBytes(std::vector<unsigned char>&  dataRange, size_t count, size_t pos = 0) const
+    virtual void writeDouble(double value, size_t pos=0)
     {
-        if (pos + count >= m_length)
+        if (pos >= m_length / sizeof(double) )
             throw DbgException("memory accessor range error");
+        
+        setDoubleFloat(m_begin + pos * sizeof(double), value);
+    }
+
+    virtual void readBytes(std::vector<unsigned char>& dataRange, size_t count, size_t pos = 0) const
+    {
+        if ( count > m_length - pos )
+            throw DbgException("memory accessor range error");
+
         dataRange = loadBytes(m_begin + pos, count);
+    }
+
+    virtual void writeBytes( const std::vector<unsigned char>&  dataRange, size_t pos=0) 
+    {
+        if ( dataRange.size() > m_length - pos )
+            throw DbgException("memory accessor range error");
+        kdlib::writeBytes(m_begin + pos, dataRange);
     }
 
     virtual void readWords(std::vector<unsigned short>& dataRange, size_t count, size_t pos = 0) const
     {
-        if ((pos + count + 1) * sizeof(unsigned short) > m_length)
+        if ( count > m_length / sizeof(unsigned short) - pos )
             throw DbgException("memory accessor range error");
+
         dataRange = loadWords(m_begin + pos* sizeof(unsigned short), count);
+    }
+
+    virtual void writeWords( const std::vector<unsigned short>&  dataRange, size_t pos=0) 
+    {
+        if ( dataRange.size() > m_length / sizeof(unsigned short) - pos )
+            throw DbgException("memory accessor range error");
+
+        kdlib::writeWords(m_begin + pos* sizeof(unsigned short), dataRange);
     }
 
     virtual void readDWords(std::vector<unsigned long>& dataRange, size_t count, size_t pos = 0) const
     {
-        if ((pos + count + 1) * sizeof(unsigned long) > m_length)
+        if ( count > m_length / sizeof(unsigned long) - pos )
             throw DbgException("memory accessor range error");
+
         dataRange = loadDWords(m_begin + pos* sizeof(unsigned long), count);
+    }
+
+    virtual void writeDWords( const std::vector<unsigned long>&  dataRange, size_t  pos=0)
+    {
+        if ( dataRange.size() > m_length / sizeof(unsigned long) - pos )
+            throw DbgException("memory accessor range error");
+
+        kdlib::writeDWords(m_begin + pos* sizeof(unsigned long), dataRange);
     }
 
     virtual void readQWords(std::vector<unsigned long long>& dataRange, size_t count, size_t pos = 0) const
     {
-        if ((pos + count + 1) * sizeof(unsigned long long) > m_length)
+        if ( count > m_length / sizeof(unsigned long long) - pos )
             throw DbgException("memory accessor range error");
+
         dataRange = loadQWords(m_begin + pos* sizeof(unsigned long long), count);
+    }
+
+    virtual void writeQWords( const std::vector<unsigned long long>&  dataRange, size_t  pos=0)
+    {
+        if ( dataRange.size() > m_length / sizeof(unsigned long long) - pos )
+            throw DbgException("memory accessor range error");
+
+        kdlib::writeQWords(m_begin + pos * sizeof(unsigned long long), dataRange);
     }
 
     virtual void readSignBytes(std::vector<char>& dataRange, size_t count, size_t pos = 0) const
     {
-        if ((pos + count + 1) * sizeof(char) > m_length)
+        if ( count > m_length - pos )
             throw DbgException("memory accessor range error");
+
         dataRange = loadSignBytes(m_begin + pos* sizeof(char), count);
+    }
+
+    virtual void writeSignBytes( const std::vector<char>&  dataRange, size_t  pos=0)
+    {
+        if ( dataRange.size() > m_length  - pos )
+            throw DbgException("memory accessor range error");
+
+        kdlib::writeSignBytes(m_begin + pos, dataRange);
     }
 
     virtual void readSignWords(std::vector<short>& dataRange, size_t count, size_t pos = 0) const
     {
-        if ((pos + count + 1) * sizeof(short) > m_length)
+        if ( count > m_length / sizeof(short) - pos )
             throw DbgException("memory accessor range error");
+
         dataRange = loadSignWords(m_begin + pos* sizeof(short), count);
+    }
+
+    virtual void writeSignWords( const std::vector<short>&  dataRange, size_t  pos=0)
+    {
+        if ( dataRange.size() > m_length / sizeof(short) - pos )
+            throw DbgException("memory accessor range error");
+
+        kdlib::writeSignWords(m_begin + pos * sizeof(short), dataRange);
     }
 
     virtual void readSignDWords(std::vector<long>& dataRange, size_t count, size_t pos = 0) const
     {
-        if ((pos + count + 1) * sizeof(long) > m_length)
+        if ( count > m_length / sizeof(long) - pos )
             throw DbgException("memory accessor range error");
+
         dataRange = loadSignDWords(m_begin + pos* sizeof(long), count);
+    }
+
+    virtual void writeSignDWords( const std::vector<long>&  dataRange, size_t  pos=0)
+    {
+        if ( dataRange.size() > m_length / sizeof(long) - pos )
+            throw DbgException("memory accessor range error");
+
+        kdlib::writeSignDWords(m_begin + pos * sizeof(long), dataRange);
     }
 
     virtual void readSignQWords(std::vector<long long>& dataRange, size_t count, size_t pos = 0) const
     {
-        if ((pos + count + 1) * sizeof(long long) > m_length)
+        if ( count > m_length / sizeof(long long) - pos )
             throw DbgException("memory accessor range error");
+
         dataRange = loadSignQWords(m_begin + pos* sizeof(long long), count);
+    }
+
+    virtual void writeSignQWords( const std::vector<long long>&  dataRange, size_t  pos=0)
+    {
+        if ( dataRange.size() > m_length / sizeof(long long) - pos )
+            throw DbgException("memory accessor range error");
+
+        kdlib::writeSignQWords(m_begin + pos * sizeof(long long), dataRange);
     }
 
     virtual void readFloats(std::vector<float>& dataRange, size_t count, size_t pos = 0) const
     {
-        if ((pos + count + 1) * sizeof(float) > m_length)
+        if ( count > m_length / sizeof(float) - pos )
             throw DbgException("memory accessor range error");
+
         dataRange = loadFloats(m_begin + pos* sizeof(float), count);
+    }
+
+    virtual void writeFloats( const std::vector<float>&  dataRange, size_t  pos=0) 
+    {
+        if ( dataRange.size() > m_length / sizeof(float) - pos )
+            throw DbgException("memory accessor range error");
+
+        kdlib::writeFloats(m_begin + pos * sizeof(float), dataRange);
     }
 
     virtual void readDoubles(std::vector<double>& dataRange, size_t count, size_t pos = 0) const
     {
-        if ((pos + count + 1) * sizeof(double) > m_length)
+        if ( count > m_length / sizeof(double) || pos > m_length / sizeof(double) - count )
             throw DbgException("memory accessor range error");
+
         dataRange = loadDoubles(m_begin + pos* sizeof(double), count);
     }
 
-    virtual DataAccessorPtr clone(size_t count, size_t pos = 0)
+    virtual void writeDoubles( const std::vector<double>&  dataRange, size_t  pos=0) 
     {
-        if (pos + count >= m_length)
+        if ( dataRange.size() > m_length / sizeof(double) - pos )
             throw DbgException("memory accessor range error");
 
-        return getMemoryAccessor(m_begin + pos, count);
+        kdlib::writeDoubles(m_begin + pos * sizeof(double), dataRange);
     }
 
     virtual MEMOFFSET_64 getAddress() const {
