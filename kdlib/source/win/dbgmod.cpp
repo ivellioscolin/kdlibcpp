dd6e011cdceff19713e4b62a92fcec8a40ee6076
SND\ussrhero_cp <SND\ussrhero_cp@9b283d60-5439-405e-af05-b73fd8c4d996>
2017-10-07 20:47:50 +0000

    fixed  bug #14138 ( failed to create module object if it's header is paged out )
    git-svn-id: https://kdlibcpp.svn.codeplex.com/svn@91262 9b283d60-5439-405e-af05-b73fd8c4d996

--- kdlib/source/win/dbgmod.cpp
+++ kdlib/source/win/dbgmod.cpp
@@ -225,29 +225,38 @@ bool isModuleUserMode( MEMOFFSET_64 baseOffset )
 
 bool isModuleManaged( MEMOFFSET_64 baseOffset )
 {
-
-    ULONG  machineType;
-    HRESULT  hres = g_dbgMgr->control->GetEffectiveProcessorType(&machineType);
-
-    ULONG64  ntHeaderOffset = baseOffset + ptrDWord( baseOffset + 0x3c );
-
-    if ( machineType == IMAGE_FILE_MACHINE_I386 )
+    try 
     {
-        IMAGE_NT_HEADERS32  ntHeader;
-        readMemory( ntHeaderOffset, &ntHeader, sizeof(ntHeader) );
 
-        return ntHeader.OptionalHeader.DataDirectory[14].Size != 0;
-    }
+        ULONG  machineType;
+        HRESULT  hres = g_dbgMgr->control->GetEffectiveProcessorType(&machineType);
+
+
+        ULONG64  ntHeaderOffset = baseOffset + ptrDWord( baseOffset + 0x3c );
+
+        if ( machineType == IMAGE_FILE_MACHINE_I386 )
+        {
+            IMAGE_NT_HEADERS32  ntHeader;
+            readMemory( ntHeaderOffset, &ntHeader, sizeof(ntHeader) );
+
+            return ntHeader.OptionalHeader.DataDirectory[14].Size != 0;
+        }
         
-    if ( machineType == IMAGE_FILE_MACHINE_AMD64 )
-    {
-        IMAGE_NT_HEADERS64  ntHeader;
-        readMemory( ntHeaderOffset, &ntHeader, sizeof(ntHeader) );
+        if ( machineType == IMAGE_FILE_MACHINE_AMD64 )
+        {
+            IMAGE_NT_HEADERS64  ntHeader;
+            readMemory( ntHeaderOffset, &ntHeader, sizeof(ntHeader) );
 
-        return ntHeader.OptionalHeader.DataDirectory[14].Size != 0;
+            return ntHeader.OptionalHeader.DataDirectory[14].Size != 0;
+        }
+
+        throw DbgException("Unknown CPU type");
+
     }
+    catch(MemoryException& )
+    {}
 
-    throw DbgException("Unknown CPU type");
+    return false;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
